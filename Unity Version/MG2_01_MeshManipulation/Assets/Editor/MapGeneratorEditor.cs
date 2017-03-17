db3b7c4738f7a11bf70e9072b2829a59b9f93e60
Matthew Duddington <mdudd001@gold.ac.uk>
2017-03-17 17:44:58 +0000

    Generation of texture from Perlin noise with adaptive variables

---
+++ Unity Version/MG2_01_MeshManipulation/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,24 @@
+﻿// Base code written while following tutorial: Procedural Landmass Generation (Unity 5) (c) Sebastian Lague 2016
+
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+  public override void OnInspectorGUI() {
+    MapGenerator mapGen = (MapGenerator)target;
+
+    if (DrawDefaultInspector ()) {
+      if (mapGen.autoUpdate) {
+        mapGen.GenerateMap ();
+      }
+    }
+
+    if (GUILayout.Button ("Generate")) {
+      mapGen.GenerateMap ();
+    }
+  }
+
+}
---
+++ Unity Version/MG2_01_MeshManipulation/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,51 @@
+﻿// Base code written while following tutorial: Procedural Landmass Generation (Unity 5) (c) Sebastian Lague 2016
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+  public int mapWidth;
+  public int mapHeight;
+  public float noiseScale;
+
+  public int octaves;
+  [Range(0,1)] public float persistance;
+  public float lacunarity;
+
+  public int seed;
+  public Vector2 offset;
+
+  public bool autoUpdate;
+
+  public void GenerateMap() {
+    float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth,
+                                                mapHeight,
+                                                seed,
+                                                noiseScale,
+                                                octaves,
+                                                persistance,
+                                                lacunarity,
+                                                offset);
+
+    MapDisplay display = FindObjectOfType<MapDisplay> ();
+    display.DrawNoiseMap (noiseMap);
+  }
+
+  void OnValidate() {
+    if (mapWidth < 1) {
+      mapWidth = 1;
+    }
+    if (mapHeight < 1) {
+      mapHeight = 1;
+    }
+    if (lacunarity < 1) {
+      lacunarity = 1;
+    }
+    if (octaves < 0) {
+      octaves = 0;
+    }
+  }
+
+}
