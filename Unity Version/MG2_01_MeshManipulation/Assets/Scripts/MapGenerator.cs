dfdb11ab5f5c34de3b98603b9e5caa1ca765987d
Matthew Duddington <mdudd001@gold.ac.uk>
2017-03-20 15:51:04 +0000

    Colour map applied to plane with variable layers

--- Unity Version/MG2_01_MeshManipulation/Assets/Scripts/MapGenerator.cs
+++ Unity Version/MG2_01_MeshManipulation/Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
+  public enum DrawMode { NoiseMap, ColourMap };
+  public DrawMode drawMode;
+
   public int mapWidth;
   public int mapHeight;
   public float noiseScale;
@@ -19,6 +22,8 @@ public class MapGenerator : MonoBehaviour {
 
   public bool autoUpdate;
 
+  public TerrainType[] regions;
+
   public void GenerateMap() {
     float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth,
                                                 mapHeight,
@@ -27,10 +32,35 @@ public class MapGenerator : MonoBehaviour {
                                                 octaves,
                                                 persistance,
                                                 lacunarity,
-                                                offset);
+                                                offset
+                                               );
+    Color[] colourMap = new Color[mapWidth * mapHeight];
+
+    for (int y = 0; y < mapHeight; y++) {
+      for (int x = 0; x < mapWidth; x++) {
+        float currentHeight = noiseMap [x, y];
+        for (int i = 0; i < regions.Length; i++) {
+          if (currentHeight <= regions [i].height) {
+            colourMap [y * mapWidth + x] = regions [i].colour;
+
+            break;
+          }
+        }
+      }
+    }
 
     MapDisplay display = FindObjectOfType<MapDisplay> ();
-    display.DrawNoiseMap (noiseMap);
+    switch (drawMode) {
+    case DrawMode.NoiseMap:
+      display.DrawTexture (TextureGenerator.TextureFromHeightMap (noiseMap));
+      break;
+    case DrawMode.ColourMap:
+      display.DrawTexture (TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
+      break;
+    default:
+      Debug.LogWarning ("No draw mode selected");
+      break;
+    }
   }
 
   void OnValidate() {
@@ -48,4 +78,11 @@ public class MapGenerator : MonoBehaviour {
     }
   }
 
+}
+
+[System.Serializable]
+public struct TerrainType {
+  public string name;
+  public float height;
+  public Color colour;
 }
